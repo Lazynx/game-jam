82673befa1bc137b51279c2c7b5124d84015274e
Farid <f7farid@gmail.com>
2024-04-18 20:59:15 +0500

    npc's are done

--- experemental.py
+++ experemental.py
@@ -35,7 +35,7 @@ for i in npc_list:
         list0.append(list1)
     npc_images.append(list0)
 resize_coef = 10/4
-backdround_image = pygame.transform.scale(backdround_image, (backdround_image.get_size()[0]*resize_coef, backdround_image.get_size()[1]*resize_coef))
+backdround_image = pygame.transform.scale(backdround_image, (12000, 12000))
 backdround_image_rect = backdround_image.get_rect()
 
 x_map = backdround_image.get_size()[0]
@@ -57,8 +57,8 @@ x_mode_1 = int()
 y_mode_1 = int()
 x_mode_2 = int()
 y_mode_2 = int()
-npc_set_mode = False
-new_npc = []
+
+
 
 
 while True:
@@ -89,16 +89,25 @@ while True:
                         if block[0] <= mouse_x + x - xx//2 <= block[0] + block[2] and block[1] <= mouse_y + y - yy//2 <= block[1] + block[3]:
                             del data[str(floor) + "-floor"]["blocks"][i]
                             break
+                    for i in range(len(data[str(floor) + "-floor"]["npc"])):
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        if (((data[str(floor) + "-floor"]["npc"][i][0] + npc_images[0][0][0].get_size()[0]//2) - mouse_x - x + xx//2)**2 + ((data[str(floor) + "-floor"]["npc"][i][1] + npc_images[0][0][0].get_size()[1]//2) - mouse_y - y +yy//2)**2)**(1/2) <= 40:
+                            del data[str(floor) + "-floor"]["npc"][i]
+                            break
             elif event.key == pygame.K_p:
-                data["1-floor"]["blocks"].clear()
-            elif event.key == pygame.K_n:
-                if npc_set_mode:
-                    npc_set_mode = False
-                    if new_npc != []:
-                        data[str(floor) + "-floor"]["npc"].append(new_npc)
-                else:
-                    npc_set_mode = True
-                    new_npc.clear()
+                if mode == 1:
+                    data["1-floor"]["npc"].clear()
+            elif event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4:
+                if mode == 1:
+                    direction = 1
+                    if event.key == pygame.K_2:
+                        direction = 2
+                    elif event.key == pygame.K_3:
+                        direction = 3
+                    elif event.key == pygame.K_4:
+                        direction = 4
+                    data["1-floor"]["npc"].append([pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2, randint(1, len(npc_images)), direction, 1, randint(0, 1000), 1000])
+                    #print([pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2, randint(1, len(npc_images)), 1, 1])
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_d:
                 x_plus_move = False
@@ -111,27 +120,13 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if mode == 1:
-                    if not npc_set_mode:
-                        x_mode_1, y_mode_1 = pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2
-                    else:
-                        if new_npc == []:
-                            new_npc.append(randint(1, len(npc_list)))
-                            new_npc.append(pygame.mouse.get_pos()[0] + x - xx//2)
-                            new_npc.append(pygame.mouse.get_pos()[1] + y - yy//2)
-                            new_npc.append(0)
-                            new_npc.append(0)
-                            new_npc.append(randint(1000,5000))
-                            new_npc.append([0, 0])
-                            new_npc.append([[pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2]])
-                            new_npc.append([1, 1, 1000])
-                        else:
-                            new_npc[7].append([pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2])
+                    x_mode_1, y_mode_1 = pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
                 if mode == 1:
-                    if not npc_set_mode:
-                        data[str(floor) + "-floor"]["blocks"].append([min(x_mode_1, x_mode_2), min(y_mode_1, y_mode_2), abs(x_mode_1 - x_mode_2), abs(y_mode_1 - y_mode_2)])
+                    data[str(floor) + "-floor"]["blocks"].append([min(x_mode_1, x_mode_2), min(y_mode_1, y_mode_2), abs(x_mode_1 - x_mode_2), abs(y_mode_1 - y_mode_2)])
     x_mode_2, y_mode_2 = pygame.mouse.get_pos()[0] + x - xx//2, pygame.mouse.get_pos()[1] + y - yy//2
+
 
     if x_plus_move:
         if x_ac <= speed:
@@ -164,6 +159,7 @@ while True:
         else:
             y_ac = 0
     
+    #print(data[str(floor) + "-floor"]["npc"])
     x_plus = True
     y_plus = True
 
@@ -217,16 +213,6 @@ while True:
         else:
             player_animation[2] +=160
     
-    for i in range(len(data[str(floor) + "-floor"]["npc"])):
-        if len(data[str(floor) + "-floor"]["npc"][i][7]) != 1:
-            if data[str(floor) + "-floor"]["npc"][i][4] >= data[str(floor) + "-floor"]["npc"][i][5]:
-                data[str(floor) + "-floor"]["npc"][i][4] = 0
-                if data[str(floor) + "-floor"]["npc"][i][3] != len(data[str(floor) + "-floor"]["npc"][i][7]) - 1:
-                    data[str(floor) + "-floor"]["npc"][i][6][0] = (data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3] + 1][0] - data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3]][0]) / ((data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3] + 1][0])**2 + (data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3]][0])**2)**(1/2)
-                    data[str(floor) + "-floor"]["npc"][i][6][1] = (data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3] + 1][1] - data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3]][1]) / ((data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3] + 1][1])**2 + (data[str(floor) + "-floor"]["npc"][i][7][data[str(floor) + "-floor"]["npc"][i][3]][1])**2)**(1/2)
-            elif data[str(floor) + "-floor"]["npc"][i][6] != []:
-                data[str(floor) + "-floor"]["npc"][i][4]+=10
-
     for i in data[str(floor) + "-floor"]["blocks"]:
         x_prev = x
         y_prev = y + y_ac
@@ -237,6 +223,25 @@ while True:
         if y_prev + player_size > i[1] and y_prev - player_size < i[1] + i[3] and x_prev + player_size > i[0] and x_prev - player_size < i[0] + i[2]:
             x_plus = False
     
+    for i in data[str(floor) + "-floor"]["npc"]:
+        if ((x - (i[0]+npc_images[0][0][0].get_size()[0]//2))**2 + (y - (i[1]+npc_images[0][0][0].get_size()[1]//2))**2)**(1/2) <= 40:
+            x_ac*=-1
+            y_ac*=-1
+    
+    for i in range(len(data[str(floor) + "-floor"]["npc"])):
+        if data[str(floor) + "-floor"]["npc"][i][5] >= data[str(floor) + "-floor"]["npc"][i][6]:
+            if data[str(floor) + "-floor"]["npc"][i][4] == 1:
+                data[str(floor) + "-floor"]["npc"][i][4] = 4
+            else:
+                data[str(floor) + "-floor"]["npc"][i][4] = 1
+            data[str(floor) + "-floor"]["npc"][i][5] = 0
+            
+        else:
+            if data[str(floor) + "-floor"]["npc"][i][4] != 4:
+                data[str(floor) + "-floor"]["npc"][i][5] += 6
+            else:
+                data[str(floor) + "-floor"]["npc"][i][5] += 180
+
     if x_plus and y_plus and x_ac and y_ac:
         x+=x_ac/(2**(1/2))
         y+=y_ac/(2**(1/2))
@@ -254,14 +259,14 @@ while True:
             for i in data[str(floor) + "-floor"]["blocks"]:
                 pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(i[0] - x + xx//2, i[1] - y + yy//2, i[2], i[3]))
             for i in data[str(floor) + "-floor"]["npc"]:
-                pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(i[1] - x + xx//2+15, i[2] - y + yy//2 + 25, 2*player_size+1, 2*player_size+1))
+                pygame.draw.circle(screen, (0, 255, 0), ((i[0]+npc_images[0][0][0].get_size()[0]//2) - x + xx//2, (i[1]+npc_images[0][0][0].get_size()[1]//2) - y + yy//2), 40)
             #screen.blit(npc_images[0][i[8][0]-1][i[8][1]-1], npc_rect)
             pygame.draw.rect(screen, (0, 0, 255), pygame.Rect(min(x_mode_1, x_mode_2) - x + xx//2, min(y_mode_1, y_mode_2) - y + yy//2, abs(x_mode_1 - x_mode_2), abs(y_mode_1 - y_mode_2)))
             pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(xx//2 - player_size, yy//2 - player_size, 2*player_size+1, 2*player_size+1))
         for i in data[str(floor) + "-floor"]["npc"]:
-            npc_rect = npc_images[0][i[8][0]-1][i[8][1]-1].get_rect()
-            npc_rect.topleft = (i[1] - x + xx//2, i[2] - y + yy//2)
-            screen.blit(npc_images[0][i[8][0]-1][i[8][1]-1], npc_rect)
+            npc_rect = npc_images[i[2]-1][i[3]-1][i[4]-1].get_rect()
+            npc_rect.topleft = (i[0] - x + xx//2, i[1] - y + yy//2)
+            screen.blit(npc_images[i[2]-1][i[3]-1][i[4]-1], npc_rect)
         player_rect = npc_images[0][player_animation[0]-1][player_animation[1]-1].get_rect()
         player_rect.center = (xx//2, yy//2)
         screen.blit(npc_images[0][player_animation[0]-1][player_animation[1]-1], player_rect)
